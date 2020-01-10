cad5b1b8b92f1499de5eedbc2d4fcf8b9203414f
pjgeutjens <pieter.geutjens@leapconsulting.be>
2020-01-10 10:58:41 +0100

    Step 2 — Creating and Hooking up the Azure Search Service

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,18 @@
-export default {}
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import searchClient from "@/services/azsearch.service";
+
+export default {
+  executeSearch({ commit }) {
+    searchClient.search(
+      "realestate-us-sample-index",
+      {
+        search: `*`,
+        count: true
+      },
+      (err, results, raw) => {
+        console.log(raw);
+      }
+    );
+  }
+};
